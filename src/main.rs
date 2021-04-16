ae77f9bb0857bb3a69535409bb77f9b9840d5679
chumachok <chumachok11@gmail.com>
2021-04-16 15:34:30 -0700

    add initial threaded implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{
     io::self,
+    thread,
     net::{TcpListener, TcpStream},
 };
 
 use futures::executor::{block_on};
+
+const NUM_WORKERS: i32 = 5;
 
 async fn handle_request(ring: &rio::Rio, stream: &TcpStream) -> io::Result<()> {
     let mut buf = vec![0_u8; 1024];
@@ -14,14 +17,33 @@ async fn handle_request(ring: &rio::Rio, stream: &TcpStream) -> io::Result<()> {
     }
 }
 
+async fn spawn_server(listener: &TcpListener) -> io::Result<()> {
+    let ring = rio::new().unwrap();
+    loop {
+        let stream = ring.accept(&listener).await.unwrap();
+        handle_request(&ring, &stream).await?;
+    }
+}
+
 fn main() -> io::Result<()> {
-    let listener = TcpListener::bind(("127.0.0.1", 3003))?;
-    let ring = rio::new()?;
+    let listener = TcpListener::bind(("127.0.0.1", 3003)).unwrap();
+    let ring = rio::new().unwrap();
+
+    for _ in 0..NUM_WORKERS {
+        let lc = listener.try_clone().unwrap();
+        thread::spawn(move || {
+            block_on(async {
+                spawn_server(&lc).await.unwrap_or_else(|err| println!("{:?}", err));
+            });
+        });
+    }
 
     block_on(async {
         loop {
             let stream = ring.accept(&listener).await.unwrap();
-            handle_request(&ring, &stream).await;
+            handle_request(&ring, &stream).await.unwrap_or_else(|err| println!("{:?}", err));
         }
-    })
+    });
+
+    Ok(())
 }
